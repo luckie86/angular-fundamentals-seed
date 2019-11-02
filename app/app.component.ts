8662713e2f432e3813a451329643e273e1d43fb5
Luka Brinar <luka.brinar@dlabs.si>
2019-11-02 15:09:10 +0100

    Add interpoaltion-expression

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
   template: `
     <div class="app">
       {{ title }}
+      <div>
+      {{ numberOne + numberTwo }}
+      </div>
+      <div>
+      {{ isHappy ? ':)' : ':(' }}
+      </div>
     </div>  
   `
 })
 export class AppComponent {
   title: string;
+  isHappy: boolean = true;
+  numberOne: number = 1;
+  numberTwo: number = 2;
   constructor() {
     this.title = 'Ultimate Angular';
   }
