478b3c1aacf6aa0bb8a3cba58c17e74962e5d62a
Luka Brinar <luka.brinar@dlabs.si>
2019-11-03 08:59:41 +0100

    Add ng-if

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,23 +5,34 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'], 
   template: `
     <div class="app">
-      <button (click)="handleClick(username.value)">
-        Get Value!
-      </button>
-      
+  
       <input 
         type="text"
-        #username
+        [value]="name"
+        (input)="handleChange($event.target.value)"
       >
-     
+      <!-- This is shorthand for -->
+
+      <div *ngIf="name.length > 2">
+          Searching for... {{ name }}
+      </div>
+      
+      <!-- for this sugar syntax -->
+      
+      <template [ngIf]="name.length > 2">
+        <div>
+          Searching for... {{ name }}
+        </div>
+      </template>
+
     </div>  
   `
 })
 export class AppComponent {
-  name: string = 'Luka';
+  name: string = '';
 
-  handleClick(value: string) {
-    console.log(value);
+  handleChange(value: string) {
+    this.name = value;
   }
   
 }
