f8da1e00bb386c306719b6c641e1b736578d5b3f
Luka Brinar <luka.brinar@dlabs.si>
2019-11-03 09:56:52 +0100

    Add ng-style

--- app/app.component.ts
+++ app/app.component.ts
@@ -38,6 +38,28 @@ interface Passenger {
             {{ i }}: {{ passenger.fullname }}
           </li>
         </ul>
+        
+        <h3>Airline Passengers</h3>
+        <ul>
+          <li *ngFor="let passenger of passengers; let i = index">
+            <span 
+            class="status"
+            [style.backgroundColor]="(passenger.checkedIn ? 'green' : 'red')">
+            </span>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </ul>
+
+        <h3>Airline Passengers</h3>
+        <ul>
+          <li *ngFor="let passenger of passengers; let i = index">
+            <span 
+            class="status"
+            [ngStyle]="{ backgroundColor: (passenger.checkedIn ? 'green' : 'red') }">
+            </span>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </ul>
 
     </div>  
   `
