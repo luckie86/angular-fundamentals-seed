7fee821deb2fca133ff9008a0f3d674cbb0c74cc
Luka Brinar <luka.brinar@dlabs.si>
2019-11-19 19:39:05 +0100

    Add observable-error-handling

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -28,16 +28,17 @@ import { Passenger } from '../../models/passenger.inteface'
 export class PassengerDashobardComponent implements OnInit {
     passengers: Passenger[];
     constructor (private passengerService: PassengerDashboardService) {}
+    
     ngOnInit() {
         this.passengerService
             .getPassengers()
-            .then((data: Passenger[]) => this.passengers = data);
+            .subscribe((data: Passenger[]) => this.passengers = data);
     }
 
     handleEdit (event: Passenger) {
         this.passengerService
             .updatePassenger(event)
-            .then((data: Passenger) => {
+            .subscribe((data: Passenger) => {
                 this.passengers = this.passengers.map((passenger: Passenger) => {
                     if (passenger.id === event.id) {
                         passenger = Object.assign({}, passenger, event);
@@ -51,7 +52,7 @@ export class PassengerDashobardComponent implements OnInit {
     handleRemove (event: Passenger) {
         this.passengerService
             .removePassenger(event)
-            .then((data: Passenger) => {
+            .subscribe((data: Passenger) => {
                 this.passengers = this.passengers.filter((passenger: Passenger) => {
                     return passenger.id !== event.id
                 });
--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { Passenger } from '../passenger-dashboard/models/passenger.inteface';
 
@@ -12,17 +14,16 @@ const PASSENGER_API: string = '/api/passengers'
 @Injectable()
 export class PassengerDashboardService {
     constructor (private http: Http) {
-        console.log(this.http);
     }
 
-    getPassengers (): Promise<Passenger[]> {
+    getPassengers (): Observable<Passenger[]> {
         return this.http
             .get(PASSENGER_API)
-            .toPromise()
-            .then((response: Response) => response.json());
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json()));
     }
 
-    updatePassenger (passenger: Passenger): Promise<Passenger> {
+    updatePassenger (passenger: Passenger): Observable<Passenger> {
         let headers = new Headers({
             'Content-Type': 'appication-json'
         });
@@ -31,15 +32,15 @@ export class PassengerDashboardService {
         })
         return this.http
             .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
-            .toPromise()
-            .then((response: Response) => response.json());
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json()));
     }
 
-    removePassenger (passenger: Passenger): Promise<Passenger> {
+    removePassenger (passenger: Passenger): Observable<Passenger> {
         return this.http
             .delete(`${PASSENGER_API}/${passenger.id}`, passenger)
-            .toPromise()
-            .then((response: Response) => response.json());
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json()));
     }
 
 }
