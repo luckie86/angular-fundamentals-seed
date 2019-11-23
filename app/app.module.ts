21f93716405d942f15e06f5b5c35f771fad457c5
Luka Brinar <luka.brinar@dlabs.si>
2019-11-23 16:36:56 +0100

    Add ngRouter

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     // angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     // custom modules
     PassengerDashboardModule,
   ],
