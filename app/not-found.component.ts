e90c632acbb73d0cdcb831131882aa6083a27648
Luka Brinar <luka.brinar@dlabs.si>
2019-11-23 16:44:31 +0100

    Add not found component

---
+++ app/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'not-found',
+    template: `
+
+        <div>
+            Not Found!
+        </div>
+
+    `
+})
+
+export class NotFoundComponent {
+    
+}
